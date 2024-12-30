c59a167bdf05e0b5f78c901de4694c12a66380e9
hon.wong <hon.wong@bytedance.com>
2024-12-29 23:34:36 -0800

    feat: continue fixing issue

--- components/index/Intro.tsx
+++ components/index/Intro.tsx
@@ -4,12 +4,12 @@ function Intro() {
   return (
     <div
       id="intro"
-      className="flex items-center bg-fixed lg:bg-contain bg-top flex-col w-full bg-no-repeat lg:h-120vh h-100vh bg-white mb-12 pt-5 lg:pt-0 repeat-0 lg:justify-end"
+      className="flex items-center bg-fixed lg:bg-contain bg-top flex-col w-full bg-no-repeat lg:h-120vh h-100vh bg-white mb-6 lg:pt-0 repeat-0 lg:justify-end"
       style={{
         backgroundImage: "url('images/intro/banner2.png')",
       }}
     >
-      <div className="justify-self-start md:justify-self-end flex flex-col lg:items-end items-start lg:justify-end justify-center">
+      <div className="justify-self-start md:justify-self-end flex flex-col lg:items-end items-start lg:justify-end justify-start mt-10">
         <img
           className="object-cover h-2/3 md:h-5/6 rounded-3xl"
           src="/images/face.png"
